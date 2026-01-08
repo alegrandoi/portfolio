70487f9dd16842d7e0ad5b0fd6280ff7ec4a7b57
Alejandro Rodriguez <zzalejandro3@gmail.com>
2026-01-08 14:28:40 +0000

    vuelvbe a darle una viuelta de porque no puede publicar la app

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -5,9 +5,7 @@ import * as React from 'react';
 import { useI18n } from '@/hooks/use-i18n';
 
 export default function Footer() {
-  const [currentYear, setCurrentYear] = React.useState(
-    new Date().getFullYear()
-  );
+  const [currentYear, setCurrentYear] = React.useState<number | null>(null);
   const { t } = useI18n();
 
   React.useEffect(() => {
@@ -18,7 +16,7 @@ export default function Footer() {
     <footer className="border-t bg-background py-6 md:px-8 md:py-0">
       <div className="container flex flex-col items-center justify-center gap-4 md:h-24 md:flex-row">
         <p className="text-balance text-center text-sm leading-loose text-muted-foreground">
-          © {currentYear} Alejandro Rodríguez Gómez. {t('footer.text')}
+          © {currentYear || new Date().getFullYear()} Alejandro Rodríguez Gómez. {t('footer.text')}
         </p>
       </div>
     </footer>
