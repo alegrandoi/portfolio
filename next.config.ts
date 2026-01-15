492e2e0df486f1b4daf7369464578d930446ab96
Alejandro Rodriguez <zzalejandro3@gmail.com>
2026-01-15 16:04:18 +0000

    failed to build: (error ID: 33537c4b): {"reason":"Failed Framework Build

--- next.config.ts
+++ next.config.ts
@@ -8,11 +8,6 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  devIndicators: {
-    allowedDevOrigins: [
-      'https://6000-firebase-studio-1759433157282.cluster-lu4mup47g5gm4rtyvhzpwbfadi.cloudworkstations.dev',
-    ],
-  },
   images: {
     remotePatterns: [
       {
